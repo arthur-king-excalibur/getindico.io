indico/indico

commit c1161884663e24fe8b5ddb4216bdf10cdeae96e4

[FIX] Clear state on column reassign
 * Was causing unnecessary column creation;

indico/htdocs/js/indico/Timetable/Layout.js
@@ -75,6 +75,7 @@ type("TimetableLayoutManager", [],
          },
 
          assign: function(assigned, block) {
+
              var ks = keys(assigned);
              ks.sort();
 
@@ -121,11 +122,15 @@ type("TimetableLayoutManager", [],
 
              // Changes the column of a block
              var reassign = function(block, col) {
+                 assigned[block.assigned] = null;
                  block.assigned = col;
                  assigned[col] = block;
+
                  if (!exists(block.sessionId)) {
                      lastAssign(block, col);
                  }
+
+                 lastAssigned[block.sessionId].col = col;
              };
 
              for (key in currentGroup) {
@@ -138,13 +143,13 @@ type("TimetableLayoutManager", [],
                      continue;
                  }
 
-                 if (!exists(lastAssigned[block.sessionId])) {
+                 if (exists(lastAssigned[block.sessionId])) {
+                     lastAssign(block);
+                 } else {
                      // This block has never been assigned before. Just update the lastAssigned.
                      lastAssign(block, block.assigned);
                      continue;
                  }
-
-                 lastAssign(block);
 
                  if (correctlyAssigned(block)) {
                      // The block has already got its prefered position
@@ -163,7 +168,6 @@ type("TimetableLayoutManager", [],
                      }
                  } else if (!exists(existingBlock.sessionId) || !exists(lastAssigned[existingBlock.sessionId]) ||
                      numAssignedBlocks(block.sessionId) > numAssignedBlocks(existingBlock.sessionId)) {
-
                      // The block currently placed in the prefered column has either no prefered column
                      // or has a preferred column but has fewer previous placed session slots (this
                      // gives lower priority).
@@ -206,7 +210,7 @@ type("TimetableLayoutManager", [],
          shouldShowRoom: function() {
              return true;
          },
-         reorderColumns:function() {
+         reorderColumns:function(group) {
          }
      }
     );
@@ -277,8 +281,8 @@ type("IncrementalLayoutManager", ["TimetableLayoutManager"],
                  // current block is [minutes, minutes + 5]
                  var startMin = (startingHour * 60 + minutes);
                  endMin = (startingHour * 60 + minutes + TimetableDefaults.resolution);
-                 var hStart = zeropad(parseInt(startMin/60, 10))+''+zeropad(startMin%60);
-                 hEnd = zeropad(parseInt(endMin/60, 10))+''+zeropad(endMin%60);
+                 var hStart = zeropad(parseInt(startMin / 60, 10)) + '' + zeropad(startMin % 60);
+                 hEnd = zeropad(parseInt(endMin / 60, 10)) + '' + zeropad(endMin % 60);
 
                  self.processTimeBlock(hStart, hEnd, startMin, minutes, algData);
              }
@@ -299,8 +303,8 @@ type("IncrementalLayoutManager", ["TimetableLayoutManager"],
                  });
                  counter++;
              });
-             return [algData.topPx, algData.grid, algData.blocks, algData.groups, algData.wholeDayBlocks];
 
+             return [algData.topPx, algData.grid, algData.blocks, algData.groups, algData.wholeDayBlocks];
          }
 
      });
@@ -446,7 +450,6 @@ type("ProportionalLayoutManager", ["IncrementalLayoutManager"],
              var pxStep = Math.floor(TimetableDefaults.layouts.proportional.values.pxPerHour*TimetableDefaults.resolution/60);
              var smallBlocks = [];
 
-
              each(points, function(point) {
 
                  if (point[1] == 'end') {
@@ -515,7 +518,9 @@ type("ProportionalLayoutManager", ["IncrementalLayoutManager"],
              }
 
              algData.topPx += pxStep;
+
          }
+
      });
 
 type("RoomLayoutManager", ["CompactLayoutManager"],

indico/htdocs/js/indico/Timetable/Draw.js
@@ -1505,12 +1505,7 @@ type("TimetableDrawer", ["IWidget", "DroppableTimetableMixin"],
 
                  var block;
 
-                 var empty = true;
-
-                 for (b in eventData.entries) {
-                     empty = false;
-                     break;
-                 }
+                 var empty = _(eventData.entries).size() > 0;
                  compactMode = false;
                  // For now don't use the compact mode. Activating it makes short entries displaying less
                  // information in the block (only time and title).
@@ -1557,7 +1552,7 @@ type("TimetableDrawer", ["IWidget", "DroppableTimetableMixin"],
              }
 
              var dayData = this.layoutChooser.get().drawDay(dayFiltered, 'session', this.startTime, this.endTime, this.managementMode);
-             var height = dayData[0]+TimetableDefaults.topMargin+TimetableDefaults.bottomMargin;
+             var height = dayData[0] + TimetableDefaults.topMargin + TimetableDefaults.bottomMargin;
              this.wrappingElement.setStyle('height', pixels(height + (this.printableVersion ? 0 : 100))); // +100 to have margin for the tabs
 
              this.grid.length = 0;

arthur-king-excalibur/getindico.io

commit 07a7ef010728fc0cdf98b066eeaa0a4cc55d369d

[FIX] Column swap in TT
 * Swapping columns was destructive;
 * Fixes problem introduced by c1161884663e24fe8b5ddb4216bdf10cdeae96e4;

indico/htdocs/js/indico/Timetable/Layout.js
@@ -125,12 +125,14 @@ type("TimetableLayoutManager", [],
                  assigned[block.assigned] = null;
                  block.assigned = col;
                  assigned[col] = block;
+                 lastAssign(block, col);
+             };
 
-                 if (!exists(block.sessionId)) {
-                     lastAssign(block, col);
-                 }
-
-                 lastAssigned[block.sessionId].col = col;
+             var swap_columns = function(block1, block2) {
+                 var block1_old_col = block1.assigned;
+                 reassign(block1, block2.assigned);
+                 reassign(block2, block1_old_col);
+                 assigned[block1.assigned] = block1;
              };
 
              for (key in currentGroup) {
@@ -176,8 +178,7 @@ type("TimetableLayoutManager", [],
                      // otherwise there might be overlapping blocks. Is there a better way
                      // to handle this so that this check is not needed?
                      if (existingBlock.start == block.start) {
-                         reassign(existingBlock, block.assigned);
-                         reassign(block, preferedCol);
+                         swap_columns(existingBlock, block)
                      }
                  }
              }