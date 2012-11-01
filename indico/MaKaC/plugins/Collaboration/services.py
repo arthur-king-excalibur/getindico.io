indico/indico

commit ae5f746beb3144c4d043260b93cc2aeb7f395b9d

[FIX] From shows wrong information.
- Now it shows the name of the user.

indico/MaKaC/plugins/Collaboration/services.py
@@ -76,7 +76,8 @@ class SendElectronicAgreement(ConferenceModifBase):
 
         self.emailToList = []
 
-        self.fromEmail = self._params['from']
+        self.fromEmail = self._params['from']['email']
+        self.fromName = self._params['from']['name']
         self.content = self._params['content']
         manager = self._conf.getCSBookingManager()
         for uniqueId in self.uniqueIdList:
@@ -111,7 +112,7 @@ class SendElectronicAgreement(ConferenceModifBase):
                 sw = manager.getSpeakerWrapperByUniqueId(uniqueId)
                 sw.setStatus(SpeakerStatusEnum.PENDING)
                 subject = """[Indico] Electronic Agreement: %s (event id: %s)"""%(self._conf.getTitle(), self._conf.getId())
-                notification = ElectroniAgreementNotification([sw.getObject().getEmail()], self.fromEmail, self.processContent(sw), subject)
+                notification = ElectroniAgreementNotification([sw.getObject().getEmail()], self.fromEmail, self.fromName, self.processContent(sw), subject)
 
                 GenericMailer.sendAndLog(notification, self._conf,
                                          "MaKaC/plugins/Collaboration/RecordingRequest/collaboration.py",

indico/MaKaC/plugins/Collaboration/mail.py
@@ -179,11 +179,11 @@ A booking / request was deleted in <a href="%s">%s</a>
 class SpeakersNotificationBase(GenericNotification):
     """ Base class to build an email notification to Speakers
     """
-    def __init__(self, sendToList, fromEmail):
+    def __init__(self, sendToList, fromEmail, fromName ="Indico Mailer"):
         GenericNotification.__init__(self)
 
         self.setContentType("text/html")
-        self.setFromAddr("Indico Mailer<%s>"%fromEmail)
+        self.setFromAddr("%s<%s>"%(fromName, fromEmail))
         self.setToList(sendToList)
 
 class ElectroniAgreementNotification(SpeakersNotificationBase):
@@ -191,7 +191,7 @@ class ElectroniAgreementNotification(SpeakersNotificationBase):
         to sign the electronic agreement.
     """
 
-    def __init__(self, sendToList, fromEmail, content, subject):
-        SpeakersNotificationBase.__init__(self, sendToList, fromEmail)
+    def __init__(self, sendToList, fromEmail, fromName, content, subject):
+        SpeakersNotificationBase.__init__(self, sendToList, fromEmail, fromName)
         self.setSubject(subject)
         self.setBody(content)

indico/MaKaC/plugins/Collaboration/pages.py
@@ -262,7 +262,7 @@ class WElectronicAgreement(wcomponents.WTemplated):
         # Here we check the rights again, and chose what contributions we should show
         requestType = CollaborationTools.getRequestTypeUserCanManage(self._conf, self._user)
 
-        self._fromList = self._user.getEmails()
+        self._fromList = [{"name": self._user.getStraightFullName() , "email": email} for email in self._user.getEmails()]
 
         contributions = manager.getContributionSpeakerByType(requestType)
 
@@ -317,7 +317,8 @@ class WElectronicAgreement(wcomponents.WTemplated):
         vars["conf"] = self._conf
         vars["contributions"] = self.getTableContent()
 
-        self._fromList.append(Config.getInstance().getNoReplyEmail())
+        self._fromList.append({"name": "Indico Mailer",
+                               "email": Config.getInstance().getNoReplyEmail()})
         vars['fromList'] = self._fromList
         manager = self._conf.getCSBookingManager()
         vars['manager'] = manager

indico/htdocs/js/indico/Collaboration/Collaboration.js
@@ -1757,36 +1757,7 @@ type("SpeakersEmailPopup", ["BasicEmailPopup"],{
             var self = this;
             return [
                 [$T('Send'), function() {
-                    var killProgress = IndicoUI.Dialogs.Util.progress($T("Sending..."));
-                    indicoRequest(
-                        "collaboration.sendElectronicAgreement",
-                        {
-                            conference: self.confId,
-                            uniqueIdList: self.uniqueIdList,
-                            from: $E("fromEmailAddress"),
-                            content: self.rtWidget.get(),
-                        },
-                        function(result, error){
-                            killProgress();
-                            if (error) {
-                                IndicoUtil.errorReport(error);
-                                self.close();
-                            } else {
-                                if(result == "url_error"){
-                                    IndicoUI.Dialogs.Util.alert($T("Email Format Error"), $T("The {url} field is missing in your email. This is a mandatory field thus, this email cannot be sent."));
-                                }else if(result == "talkTitle_error"){
-                                    IndicoUI.Dialogs.Util.alert($T("Email Format Error"), $T("The {talkTitle} field is missing in your email. This is a mandatory field thus, this email cannot be sent."));
-                                }else{
-                                    self.close();
-                                    var divs = $('<div/>');
-                                    $('<div/>').html($T("Email sent successfully!")).appendTo(divs);
-                                    $('<div/>').html($T("The email has been sent to:")).appendTo(divs);
-                                    $('<div/>').text(result).appendTo(divs);
-                                    informationPopup(divs);
-                                }
-                            }
-                        }
-                    );
+                    self.sendFunction();
                 }],
                 [$T('Cancel'), function() {
                     self.close();
@@ -1798,8 +1769,8 @@ type("SpeakersEmailPopup", ["BasicEmailPopup"],{
         var self = this;
         // drop down list with senders
         optionList = Array();
-        for(var i in self.fromList){
-            optionList.push(Html.option({value: self.fromList[i]}, "Indico Mailer <"+self.fromList[i]+">"));
+        for(i in self.fromList){
+            optionList.push(Html.option({value: self.fromList[i].email}, self.fromList[i].name + " <"+self.fromList[i].email+">"));
         }
         var selectFromAddress = Html.select({id:"fromEmailAddress"}, optionList);
 
@@ -1829,15 +1800,18 @@ type("SpeakersEmailPopup", ["BasicEmailPopup"],{
         self.uniqueIdList = uniqueIdList;
         self.fromList = fromList;
         self.sendFunction=function(){
+            var killProgress = IndicoUI.Dialogs.Util.progress($T("Sending..."));
             indicoRequest(
                 "collaboration.sendElectronicAgreement",
                 {
                     conference : self.confId,
                     uniqueIdList: self.uniqueIdList,
-                    from: $E("fromEmailAddress"),
+                    from: {email: self.fromList[$("#fromEmailAddress")[0].selectedIndex].email,
+                        name: self.fromList[$("#fromEmailAddress")[0].selectedIndex].name },
                     content: self.rtWidget.get()
                 },
                 function(result, error){
+                    killProgress();
                     if (error) {
                         IndicoUtil.errorReport(error);
                         self.close();

arthur-king-excalibur/getindico.io

commit 2da8e46cbed9b92ebad736c305d44532341fdd6c

[FIX] Fixed unit test
 * Broken by ae5f746beb3144c4d043260b93cc2aeb7f395b9d;

indico/MaKaC/plugins/Collaboration/services.py
@@ -84,7 +84,7 @@ class SendElectronicAgreement(ConferenceModifBase):
         self.fromEmail = self._params['from']['email']
         self.fromName = self._params['from']['name']
         self.content = self._params['content']
-        p_cc = self._params.get('cc').strip()
+        p_cc = self._params.get('cc', '').strip()
         self.cc = setValidEmailSeparators(p_cc).split(',') if p_cc else []
         manager = self._conf.getCSBookingManager()
         for uniqueId in self.uniqueIdList:

indico/tests/python/unit/plugins_tests/Collaboration_tests/electronicAgreement_test.py
@@ -313,14 +313,17 @@ class TestElectronicAgreement(IndicoTestCase):
         service._getAnswer()
 
     def sendEmailService(self, uniqueId):
-        fromField = "no-reply@test.ch"
+        fromField = {
+            "name": "No-reply",
+            "email": "no-reply@test.ch"
+            }
         content = "This is a test {url} {talkTitle}..."
 
         params = {
-                  'from':fromField,
-                  'content':content,
-                  'uniqueIdList':uniqueId,
-                  'confId':self._conf.getId()
+                  'from': fromField,
+                  'content': content,
+                  'uniqueIdList': uniqueId,
+                  'confId': self._conf.getId()
                   }
         service = SendElectronicAgreement(params, self.falseSession, self.falseSession)
         service._checkParams()