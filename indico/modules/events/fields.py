indico/indico

commit 683efb9ff80c29c70be3d1fa25372fa6a5e058f7

Move EventPerson logic to util func

indico/modules/events/fields.py
@@ -27,8 +27,8 @@ from indico.core.errors import UserValueError
 from indico.modules.events.layout import theme_settings
 from indico.modules.events.models.persons import EventPerson, EventPersonLink, PersonLinkBase
 from indico.modules.events.models.references import ReferenceType
+from indico.modules.events.persons.util import get_event_person
 from indico.modules.events.util import serialize_person_link
-from indico.modules.users import User
 from indico.modules.users.models.users import UserTitle
 from indico.modules.users.util import get_user_by_email
 from indico.util.i18n import _
@@ -98,46 +98,14 @@ class EventPersonListField(PrincipalListField):
         return getattr(self.get_form(), 'event', None)
 
     def _convert_data(self, data):
-        return map(self._get_event_person, data)
-
-    def _create_event_person(self, data):
-        title = next((x.value for x in UserTitle if data.get('title') == x.title), None)
-        person = EventPerson(event=self.event, email=data.get('email', '').lower(), _title=title,
-                             first_name=data.get('firstName'), last_name=data['familyName'],
-                             affiliation=data.get('affiliation'), address=data.get('address'),
-                             phone=data.get('phone'), is_untrusted=self.create_untrusted_persons)
-        # Keep the original data to cancel the conversion if the person is not persisted to the db
-        self.event_person_conversions[person] = data
-        return person
-
-    def _get_event_person_for_user(self, user):
-        person = EventPerson.for_user(user, self.event, is_untrusted=self.create_untrusted_persons)
-        # Keep a reference to the user to cancel the conversion if the person is not persisted to the db
-        self.event_person_conversions[person] = user
-        return person
-
-    def _get_event_person(self, data):
-        person_type = data.get('_type')
-        if person_type is None:
-            if data.get('email'):
-                email = data['email'].lower()
-                user = User.find_first(~User.is_deleted, User.all_emails.contains(email))
-                if user:
-                    return self._get_event_person_for_user(user)
-                elif self.event:
-                    person = self.event.persons.filter_by(email=email).first()
-                    if person:
-                        return person
-            # We have no way to identify an existing event person with the provided information
-            return self._create_event_person(data)
-        elif person_type == 'Avatar':
-            return self._get_event_person_for_user(self._convert_principal(data))
-        elif person_type == 'EventPerson':
-            return self.event.persons.filter_by(id=data['id']).one()
-        elif person_type == 'PersonLink':
-            return self.event.persons.filter_by(id=data['personId']).one()
-        else:
-            raise ValueError(_("Unknown person type '{}'").format(person_type))
+        result = []
+        for person_data in data:
+            person = get_event_person(person_data, create_untrusted_persons=self.create_untrusted_persons,
+                                      allow_pending=self.allow_external, allow_emails=self.allow_emails,
+                                      allow_networks=self.allow_networks, existing_data=self.object_data)
+            self.event_person_conversions[person] = data
+            result.append(person)
+        return map(get_event_person, data)
 
     def _serialize_principal(self, principal):
         from indico.modules.events.util import serialize_event_person
@@ -182,7 +150,7 @@ class PersonLinkListFieldBase(EventPersonListField):
     @no_autoflush
     def _get_person_link(self, data, extra_data=None):
         extra_data = extra_data or {}
-        person = self._get_event_person(data)
+        person = get_event_person(self.event, data, create_untrusted_persons=self.create_untrusted_persons)
         person_data = {'title': next((x.value for x in UserTitle if data.get('title') == x.title), UserTitle.none),
                        'first_name': data.get('firstName', ''), 'last_name': data['familyName'],
                        'affiliation': data.get('affiliation', ''), 'address': data.get('address', ''),

indico/modules/events/persons/util.py
@@ -1,0 +1,69 @@
+# This file is part of Indico.
+# Copyright (C) 2002 - 2017 European Organization for Nuclear Research (CERN).
+#
+# Indico is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 3 of the
+# License, or (at your option) any later version.
+#
+# Indico is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Indico; if not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import unicode_literals
+
+from indico.modules.events.models.persons import EventPerson
+from indico.modules.users import User
+from indico.modules.users.models.users import UserTitle
+from indico.util.user import principal_from_fossil
+
+
+def create_event_person(event, create_untrusted_persons=False, **data):
+    """Create an event person from data passed as kwargs."""
+    title = next((x.value for x in UserTitle if data.get('title') == x.title), None)
+    return EventPerson(event=event, email=data.get('email', '').lower(), _title=title,
+                       first_name=data.get('firstName'), last_name=data['familyName'],
+                       affiliation=data.get('affiliation'), address=data.get('address'),
+                       phone=data.get('phone'), is_untrusted=create_untrusted_persons)
+
+
+def get_event_person_for_user(event, user, create_untrusted_persons=False):
+    """Return the event person that links to a given User/Event (if any)."""
+    return EventPerson.for_user(user, event, is_untrusted=create_untrusted_persons)
+
+
+def get_event_person(event, data, create_untrusted_persons=False, allow_external=False, allow_emails=False,
+                     allow_networks=False):
+    """Get an EventPerson from dictionary data.
+
+    If there is already an event person in the same event and for the same user,
+    it will be returned. Matching is done with the e-mail.
+    """
+    person_type = data.get('_type')
+    if person_type is None:
+        if data.get('email'):
+            email = data['email'].lower()
+            user = User.find_first(~User.is_deleted, User.all_emails.contains(email))
+            if user:
+                return get_event_person_for_user(event, user, create_untrusted_persons=create_untrusted_persons)
+            elif event:
+                person = event.persons.filter_by(email=email).first()
+                if person:
+                    return person
+        # We have no way to identify an existing event person with the provided information
+        return create_event_person(event, create_untrusted_persons=create_untrusted_persons, **data)
+    elif person_type == 'Avatar':
+        # XXX: existing_data
+        principal = principal_from_fossil(data, allow_pending=allow_external, allow_emails=allow_emails,
+                                          allow_networks=allow_networks)
+        return get_event_person_for_user(event, principal, create_untrusted_persons=create_untrusted_persons)
+    elif person_type == 'EventPerson':
+        return event.persons.filter_by(id=data['id']).one()
+    elif person_type == 'PersonLink':
+        return event.persons.filter_by(id=data['personId']).one()
+    else:
+        raise ValueError("Unknown person type '{}'".format(person_type))

indico/modules/events/persons/util_test.py
@@ -1,0 +1,131 @@
+# This file is part of Indico.
+# Copyright (C) 2002 - 2017 European Organization for Nuclear Research (CERN).
+#
+# Indico is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 3 of the
+# License, or (at your option) any later version.
+#
+# Indico is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Indico; if not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import unicode_literals
+
+import pytest
+
+from indico.modules.events.persons.util import create_event_person, get_event_person, get_event_person_for_user
+
+
+def test_get_person_for_user(db, dummy_event, dummy_user):
+    person = get_event_person_for_user(dummy_event, dummy_user)
+    # EventPerson created from scratch
+    assert person.id is None
+    assert person.user == dummy_user
+    db.session.add(person)
+    db.session.flush()
+    person = get_event_person_for_user(dummy_event, dummy_user)
+    # Already existing EventPerson
+    assert person.id is not None
+    assert person.user == dummy_user
+
+
+def test_create_event_person(db, dummy_event):
+    data = {
+        'email': 'test@acme.com',
+        'firstName': 'John',
+        'familyName': 'Doe',
+        'affiliation': 'ACME Inc.'
+    }
+    person = create_event_person(dummy_event, **data)
+    assert person.event == dummy_event
+    assert person.email == 'test@acme.com'
+    assert person.full_name == 'John Doe'
+    assert person.affiliation == 'ACME Inc.'
+
+
+def test_get_event_person(db, dummy_event, dummy_user):
+    data = {
+        'email': 'test@acme.com',
+        'firstName': 'John',
+        'familyName': 'Doe',
+        'affiliation': 'ACME Inc.'
+    }
+    person_1 = get_event_person(dummy_event, data)
+    # Person doesn't exist in the DB
+    assert person_1.id is None
+    # User neither
+    assert person_1.user is None
+    # save in the DB for later
+    db.session.add(person_1)
+    db.session.flush()
+
+    data = {
+        'email': '1337@example.com',
+        'firstName': 'Sea',
+        'familyName': 'Pig',
+        'affiliation': 'ACME Inc.'
+    }
+    person_2 = get_event_person(dummy_event, data)
+    # Person not in the DB either
+    assert person_2.id is None
+    # User exists, however (matched by e-mail)
+    assert person_2.user == dummy_user
+    assert person_2.full_name == 'Guinea Pig'
+
+    db.session.add(person_2)
+    db.session.flush()
+    person = get_event_person(dummy_event, data)
+    # Retrieved person should now be in the DB
+    assert person.id == person_2.id
+
+    # User for whom there is already an EventPerson in this event
+    data = {
+        'email': 'test@acme.com',
+        'firstName': 'JOHN',
+        'familyName': 'DOE',
+        'affiliation': 'ACME'
+    }
+    person_3 = get_event_person(dummy_event, data)
+    # We should get the first person
+    assert person_3.id == person_1.id
+    assert person_3.user is None
+    assert person_3.full_name == 'John Doe'
+
+    data = {
+        'firstName': 'Foo',
+        'familyName': 'Bar'
+    }
+    person_4 = get_event_person(dummy_event, data)
+    # We should get a new person
+    assert person_4.id is None
+    assert person_4.user is None
+    assert person_4.email == ''
+    assert person_4.full_name == 'Foo Bar'
+
+
+def test_get_event_person_edit(db, dummy_event, dummy_user):
+    data = {
+        'email': 'test@acme.com',
+        'firstName': 'John',
+        'familyName': 'Doe',
+        'affiliation': 'ACME Inc.'
+    }
+    person_1 = get_event_person(dummy_event, dict(data, _type='Avatar', id=dummy_user.id))
+    assert person_1.id is None
+    assert person_1.user == dummy_user
+    db.session.add(person_1)
+    db.session.flush()
+
+    person_2 = get_event_person(dummy_event, dict(data, _type='EventPerson', id=person_1.id))
+    assert person_2 == person_1
+
+    person_3 = get_event_person(dummy_event, dict(data, _type='PersonLink', personId=person_1.id))
+    assert person_3 == person_1
+
+    with pytest.raises(ValueError):
+        get_event_person(dummy_event, dict(data, _type='UnsupportedPersonType'))

arthur-king-excalibur/getindico.io

commit 95c0e8f46a8c61c7f515ce1c49e31a2b67d58fac

Fix selecting external users in person links
This broke during a refactoring in 683efb9ff80
see #3562

indico/modules/events/fields.py
@@ -143,7 +143,8 @@ class PersonLinkListFieldBase(EventPersonListField):
     @no_autoflush
     def _get_person_link(self, data, extra_data=None):
         extra_data = extra_data or {}
-        person = get_event_person(self.event, data, create_untrusted_persons=self.create_untrusted_persons)
+        person = get_event_person(self.event, data, create_untrusted_persons=self.create_untrusted_persons,
+                                  allow_external=True)
         person_data = {'title': next((x.value for x in UserTitle if data.get('title') == orig_string(x.title)),
                                      UserTitle.none),
                        'first_name': data.get('firstName', ''), 'last_name': data['familyName'],